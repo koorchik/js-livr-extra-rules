00de660bf9dc44b295be4e7509bc4dd56e21dfcd
Vera <bepa.rdnv@gmail.com>
2018-01-12 17:20:41 +0200

    Add positive iso_date tests

--- src/rules/iso_date.js
+++ src/rules/iso_date.js
@@ -1,8 +1,7 @@
 var util = require('../util');
-var isoDateRe = /^([0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])(T(2[0-3]|[01][0-9]):([0-5][0-9])(:([0-5][0-9])(\.[0-9]+)?)?)?(Z|[\+\-](2[0-3]|[01][0-9]):([0-5][0-9]))?$/;
+var isoDateRe = /^([0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])(T(2[0-3]|[01][0-9]):([0-5][0-9])(:([0-5][0-9])(\.[0-9]+)?)?(Z|[\+\-](2[0-3]|[01][0-9]):([0-5][0-9]))?)?$/;
 var isoDateFormats = [ "date", "datetime" ];
 var isoDateSpecialDates = [ "yesterday", "current", "tomorrow" ];
-var isoDateSpecialAdds = [ -24 * 60 * 60 * 1000, 0, 24 * 60 * 60 * 1000 ];
 
 function iso_date(params) {
     var min;
@@ -19,7 +18,7 @@ function iso_date(params) {
         if ( util.isNoValue(value) ) return;
         if (!util.isPrimitiveValue(value)) return 'FORMAT_ERROR';
 
-        if (!(value+'').match(isoDateRe) ) return 'WRONG_DATE';
+        if (!(value+'').match(isoDateRe)) return 'WRONG_DATE';
 
         var epoch = Date.parse(value);
         if (!epoch && epoch !== 0 ) return 'WRONG_DATE';
@@ -28,8 +27,9 @@ function iso_date(params) {
         if ( max && epoch > max ) return 'DATE_TOO_HIGH';
 
         var date = new Date(epoch);
+
         if ( format === "date" ) {
-            outputArr.push(date.toISOString());
+            outputArr.push(formatDate(date));
         } else {
             outputArr.push(date.toISOString());
         }
@@ -61,22 +61,33 @@ function getDateFromParams(param, key) {
         
         date = new Date(epoch);
     }
-        
+
     if (!matched || !matched[4]) {
         date.setHours(0);
         date.setMinutes(0);
         date.setSeconds(0);
         date.setMilliseconds(0);
-            
+
         if (key === 'max') {
             date.setDate(date.getDate() + 1);
             date.setTime(date.getTime() - 1);
         }
-    } else if (matched && matched[10] === 'Z') {
+    } else if (matched[4] && (matched[10] === 'Z' || !matched[10])) {
         return date.getTime();
     }
 
     return date.getTime() - date.getTimezoneOffset() * 60 * 1000;
 }
+
+function formatDate(date) {
+    var yyyy = date.getFullYear().toString();
+    var mm = (date.getMonth()+1).toString();
+    var dd  = date.getDate().toString();
+  
+    var mmChars = mm.split('');
+    var ddChars = dd.split('');
+  
+    return yyyy + '-' + (mmChars[1] ? mm : '0'+mmChars[0]) + '-' + (ddChars[1] ? dd : '0'+ddChars[0]);
+  }
 
 module.exports = iso_date;
