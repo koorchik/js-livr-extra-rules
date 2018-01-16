ee29954ededb85b6d120007dda27d4ae65c705d6
Vera <bepa.rdnv@gmail.com>
2018-01-16 11:46:13 +0200

    Update required_if rule

--- src/rules/required_if.js
+++ src/rules/required_if.js
@@ -1,35 +1,36 @@
 var util = require('../util');
 
 function required_if(query) {
-    var entry;
+    var key;
+    var queryValue;
 
     if (arguments.length > 1) {
-        entry = Object.entries(query);
-        entry = entry[0];
+        key = Object.keys(query)[0];
+        queryValue = query[key];
         
-        if (!util.isPrimitiveValue(entry[1])) {
+        if (!util.isPrimitiveValue(query[key])) {
             throw new Error('LIVR: the target value of the "require_if" rule is incomparable');
         }
         
-        if (entry[0].split('/').length > 1) {
-            entry[0] = entry[0].split('/');
+        if (key.split('/').length > 1) {
+            key = key.split('/');
         }
     }
 
     return function(value, params) {
-        if (!util.isNoValue(value) || !entry) return;
+        if (!util.isNoValue(value) || !queryValue) return;
 
         var checkValue;
 
-        if (Array.isArray(entry[0])) {
+        if (Array.isArray(key)) {
             checkValue = params;
             
-            for (var i = 0; i < entry[0].length; i++) {
-                checkValue = checkValue[entry[0][i]];
+            for (var i = 0; i < key.length; i++) {
+                checkValue = checkValue[key[i]];
             }
-        } else checkValue = params[entry[0]];
+        } else checkValue = params[key];
 
-        if ( checkValue == entry[1] && util.isNoValue(value)) return 'REQUIRED';
+        if ( checkValue == queryValue && util.isNoValue(value)) return 'REQUIRED';
 
         return;
     }
