d05a0557b655c600f2fdf6e6fe445acecf64d3f3
Vera <bepa.rdnv@gmail.com>
2018-01-18 16:53:50 +0200

    Add is rule

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ module.exports = {
     'boolean': require('./rules/boolean'),
     'credit_card': require('./rules/credit_card'),
     'ipv4': require('./rules/ipv4'),
+    'is': require('./rules/is'),
     'iso_date': require('./rules/iso_date'),
     'list_items_unique': require('./rules/list_items_unique'),
     'list_length': require('./rules/list_length'),
